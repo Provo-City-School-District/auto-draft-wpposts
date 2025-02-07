1a5adb389bbb4094c071e75bdb8a8e4177a1c622
Josh Espinoza <joshespi@gmail.com>
2025-02-07 06:54:12 -0700

    log to syslog rather then local file

--- draft-old-posts.sh
+++ draft-old-posts.sh
@@ -12,16 +12,16 @@ run_query() {
     local DB_NAME=$2
     local SQL_QUERY=$3
 
-    echo "Running query for DB_GROUP: $DB_GROUP on database: $DB_NAME" >> $LOGFILE
+    logger "Running query for DB_GROUP: $DB_GROUP on database: $DB_NAME" >> $LOGFILE
 
     # Run the query using MySQL client, referencing the group in .my.cnf
     mysql --defaults-file=.my.cnf --defaults-group-suffix="_$DB_GROUP" "$DB_NAME" -e "$SQL_QUERY"
     QUERY_EXIT_CODE=$?
 
     if [ $QUERY_EXIT_CODE -eq 0 ]; then
-        echo "[$(date)] Query successful for $DB_GROUP on database: $DB_NAME." >> $LOGFILE
+        logger "[$(date)] Query successful for $DB_GROUP on database: $DB_NAME." >> $LOGFILE
     else
-        echo "[$(date)] Error in $DB_GROUP on database: $DB_NAME" >> $LOGFILE
+        logger "[$(date)] Error in $DB_GROUP on database: $DB_NAME" >> $LOGFILE
     fi
 }
 
@@ -30,10 +30,10 @@ while IFS='|' read -r DB_GROUP DB_NAME SQL_QUERY; do
     # Skip empty lines or lines starting with a comment (#)
     [[ -z "$DB_GROUP" || "$DB_GROUP" =~ ^# ]] && continue
 
-    echo "Processing line: $DB_GROUP on database: $DB_NAME" >> $LOGFILE
+    logger "Processing line: $DB_GROUP on database: $DB_NAME" >> $LOGFILE
 
     # Run the query for each database
     run_query "$DB_GROUP" "$DB_NAME" "$SQL_QUERY"
 done < "$CONFIG_FILE"
 
-echo "[$(date)] Monthly post drafting completed." >> $LOGFILE
+logger "[$(date)] Monthly post drafting completed." >> $LOGFILE
